5d48cb32c3e021cbb6b5e61e502ba6fcd34a4231
Lacoste Mathieu <mathieu@MBP-de-Lacoste.lan>
2018-08-24 17:03:17 +0200

    easy admin posé

--- src/MaParcelleDeBonheurBundle/Controller/DefaultController.php
+++ src/MaParcelleDeBonheurBundle/Controller/DefaultController.php
@@ -8,54 +8,31 @@ class DefaultController extends Controller
 {
     public function homepageAction()
     {
-        $titrePage = "Ma parcelle de bonheur";
-        return $this->render('@MaParcelleDeBonheur/Default/homepage.html.twig', array(
-            'titrepage' => $titrePage,
-        ));
+        return $this->render('@MaParcelleDeBonheur/Default/homepage.html.twig');
     }
 
     public function leconceptAction()
     {
-        $titrePage = "Le concept";
-        return $this->render('@MaParcelleDeBonheur/Default/leconcept.html.twig', array(
-            'titrepage' => $titrePage,
-        ));
+        return $this->render('@MaParcelleDeBonheur/Default/leconcept.html.twig');
     }
 
     public function maparcelleAction()
     {
-        $titrePage = "Ma parcelle";
-        return $this->render('@MaParcelleDeBonheur/Default/maparcelle.html.twig', array(
-            'titrepage' => $titrePage,
-        ));
+        return $this->render('@MaParcelleDeBonheur/Default/maparcelle.html.twig');
     }
 
     public function lekitdujardinierAction()
     {
-        $titrePage = "Mon kit du jardinier";
-        return $this->render('@MaParcelleDeBonheur/Default/lekitdujardinier.html.twig', array(
-            'titrepage' => $titrePage,
-        ));
+        return $this->render('@MaParcelleDeBonheur/Default/lekitdujardinier.html.twig');
     }
 
     public function lesplusdupatronAction()
     {
-        $titrePage = "Les Plus du patron";
-        return $this->render('@MaParcelleDeBonheur/Default/lesplusdupatron.html.twig', array(
-            'titrepage' => $titrePage,
-        ));
+        return $this->render('@MaParcelleDeBonheur/Default/lesplusdupatron.html.twig');
     }
 
     public function reserverAction()
     {
-        $titrePage = "Contact";
-        return $this->render('@MaParcelleDeBonheur/Default/reservation.html.twig', array(
-            'titrepage' => $titrePage,
-        ));
-    }
-
-    public function adminAction()
-    {
-        return $this->render('@MaParcelleDeBonheur/Default/admin.html.twig');
+        return $this->render('@MaParcelleDeBonheur/Default/reservation.html.twig');
     }
 }
